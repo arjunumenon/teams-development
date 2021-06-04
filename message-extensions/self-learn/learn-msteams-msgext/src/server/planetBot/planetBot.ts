435de575090c09423989af672124c43ba1b8dd86
Arjun Menon <arjun.umenon@gmail.com>
2021-06-04 19:53:27 +0530

    Updated with Search Messaging extension

--- message-extensions/self-learn/learn-msteams-msgext/src/server/planetBot/planetBot.ts
+++ message-extensions/self-learn/learn-msteams-msgext/src/server/planetBot/planetBot.ts
@@ -2,7 +2,8 @@ import {
   TeamsActivityHandler,
   TurnContext,
   MessageFactory,
-  CardFactory, MessagingExtensionAction, MessagingExtensionActionResponse, MessagingExtensionAttachment
+  CardFactory, MessagingExtensionAction, MessagingExtensionActionResponse, MessagingExtensionAttachment,
+  MessagingExtensionQuery, MessagingExtensionResponse
   } from "botbuilder";
   import { find, sortBy } from "lodash";
   
@@ -91,6 +92,53 @@ import {
     
       // return the adaptive card
       return CardFactory.adaptiveCard(adaptiveCardSource);
+    }
+
+    protected handleTeamsMessagingExtensionQuery(context: TurnContext, query: MessagingExtensionQuery): Promise<MessagingExtensionResponse> {
+      // get the search query
+      let searchQuery = "";
+      if (query && query.parameters && query.parameters[0].name === "searchKeyword" && query.parameters[0].value) {
+        searchQuery = query.parameters[0].value.trim().toLowerCase();
+      }
+    
+      // load planets
+      const planets: any = require("./planets.json");
+      // search results
+      let queryResults: string[] = [];
+    
+      switch (searchQuery) {
+        case "inner":
+          // get all planets inside asteroid belt
+          queryResults = planets.filter((planet) => planet.id <= 4);
+          break;
+        case "outer":
+          // get all planets outside asteroid belt
+          queryResults = planets.filter((planet) => planet.id > 4);
+          break;
+        default:
+          // get the specified planet
+          queryResults.push(planets.filter((planet) => planet.name.toLowerCase() === searchQuery)[0]);
+      }
+    
+      // get the results as cards
+      const searchResultsCards: MessagingExtensionAttachment[] = [];
+      queryResults.forEach((planet) => {
+        searchResultsCards.push(this.getPlanetResultCard(planet));
+      });
+    
+      const response: MessagingExtensionResponse = {
+        composeExtension: {
+          type: "result",
+          attachmentLayout: "list",
+          attachments: searchResultsCards
+        }
+      } as MessagingExtensionResponse;
+    
+      return Promise.resolve(response);
+    }
+
+    private getPlanetResultCard(selectedPlanet: any): MessagingExtensionAttachment {
+      return CardFactory.heroCard(selectedPlanet.name, selectedPlanet.summary, [selectedPlanet.imageLink]);
     }
 
   }
